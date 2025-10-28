695a78f9c6e7bf75dcafb2ff631fcd1accd5e3ae
Azwad Jahan <azwadjahan77@gmail.com>
2025-10-28 23:24:15 +0530

    Added Mobile nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ function Footer() {
                 <div>
                     <h1 className="font-bold text-white">Links</h1>
                     <ul className="text-neutral-500 flex flex-col gap-4 mt-4">
-                        <li><a href="/home">Home</a></li>
+                        <li><a href="/">Home</a></li>
                         <li><a href="/about">About</a></li>
                         <li><a href="/blogs">Blogs</a></li>
                         <li><a href="/news">News</a></li>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
 import { useLenis } from "lenis/react";
 import codenix_full_logo_white from "../assets/codenix_full_logo_white.png";
+import logo_white from "../assets/codenix_single.svg";
+import { GiCrossMark } from "react-icons/gi";
+import "./Nav.css";
+
 
 function LI({ children }) {
     return (
@@ -15,25 +21,108 @@ function LI({ children }) {
     );
 }
 
-function Nav() {
+function MobileNav({ isOpen, onClose }) {
+    if (!isOpen) {
+        // Re-enable scrolling when nav is closed
+        if (typeof document !== 'undefined') {
+            document.body.style.overflow = 'unset';
+        }
+        return null;
+    }
+
+    // Prevent scrolling when nav is open
+    if (typeof document !== 'undefined') {
+        document.body.style.overflow = 'hidden';
+    }
+
     return (
-        <div className="hidden xl:flex justify-between items-center p-4 h-10vh">
-            <img
-                src={codenix_full_logo_white}
-                alt="codenix_logo"
-                className="h-auto w-[12%]"
-            />
-            <nav className="">
-                <ul className="flex justify-end pr-4 pt-2 text-white gap-4">
-                    <LI><a href="/">Home</a></LI>
-                    <LI><a href="/activity">Activity</a></LI>
-                    <LI><a href="/news">News</a></LI>
-                    <LI><a href="/#gallery">Gallery</a></LI>
-                    <LI><a href="/blogs">Blogs</a></LI>
-                    <LI><a href="/about">About</a></LI>
-                </ul>
-            </nav>
-        </div>
+        createPortal(<>
+            <div
+                className="z-1000 fixed inset-0 bg-black/70 backdrop-blur-sm"
+            >
+                <div
+                    className="
+                fixed
+            top-0 left-0 w-full
+                bg-black-10
+                p-6
+                overflow-y-auto
+            "
+                >
+                    <div className="flex">
+                        <span className="grow"></span>
+                        <button onClick={() => onClose()} className="text-red-500 text-xl">
+                            <GiCrossMark />
+                        </button>
+                    </div>
+                    <hr className="my-3 text-neutral-600/10" />
+                    <div>
+                        <img src={logo_white} alt="" className="w-8"/>
+                    </div>
+                    <div className="
+                        mobileNav
+                        text-center text-white mt-8 grid grid-cols-2 gap-3
+                    ">
+                        <a href="/">Home</a>
+                        <a href="/activity">Activity</a>
+                        <a href="/news">News</a>
+                        <a href="/#gallery">Gallery</a>
+                        <a href="/blogs">Blogs</a>
+                        <a href="/about">About</a>
+                    </div>
+                </div>
+            </div>
+
+        </>, document.body)
+    )
+}
+
+function Nav() {
+    const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+
+    return (
+        <>
+            <MobileNav isOpen={isMobileNavOpen} onClose={() => setMobileNavOpen(false)}/>
+            <div className="hidden xl:flex justify-between items-center p-4 h-10vh">
+                <img
+                    src={codenix_full_logo_white}
+                    alt="codenix_logo"
+                    className="h-auto w-[12%]"
+                />
+                <nav className="">
+                    <ul className="flex justify-end pr-4 pt-2 text-white gap-4">
+                        <LI><a href="/">Home</a></LI>
+                        <LI><a href="/activity">Activity</a></LI>
+                        <LI><a href="/news">News</a></LI>
+                        <LI><a href="/#gallery">Gallery</a></LI>
+                        <LI><a href="/blogs">Blogs</a></LI>
+                        <LI><a href="/about">About</a></LI>
+                    </ul>
+                </nav>
+            </div>
+
+            <div className="xl:hidden flex justify-between items-center p-4 h-10vh">
+                <img src={logo_white} alt="" className="w-10" />
+                <button
+                    onClick={() => setMobileNavOpen(true)}
+                    className="
+                        bg-black-30 rounded-full
+                        py-1 px-4 flex items-center
+                        gap-2 active:scale-95
+                        transition-transform duration-100
+                    "
+                >
+                    <span className="font-bold text-sm text-neutral-300">
+                        CODÉNIX
+                    </span>
+                    <span className="p-px h-4 bg-neutral-500 rounded-full"></span>
+                    <span className="flex flex-col items-end justify-center gap-1">
+                        <span className="p-px rounded-full w-5 bg-neutral-300"></span>
+                        <span className="p-px rounded-full w-3 bg-neutral-300"></span>
+                    </span>
+                </button>
+            </div>
+        </>
     );
 }
 
